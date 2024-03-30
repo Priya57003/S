f6187fe9d94a7daeffc87efed6f3eabac26acfe4
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2024-03-30 23:41:30 +0530

    Create BookRepo.java

---
+++ BOOKAPI/BookRepo.java
@@ -0,0 +1,13 @@
+package com.APIpractice.APIpractice.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.APIpractice.APIpractice.entity.Book;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book, Integer>{
+
+	
+
+}
