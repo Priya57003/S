77024f554f5c77eb55bd7c7622898213c56aac0d
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2024-04-03 17:47:10 +0530

    Delete 00-starter-sql-scripts directory

--- 00-starter-sql-scripts/01-create-user.sql
+++
@@ -1,7 +0,0 @@
--- Drop user first if they exist
-DROP USER if exists 'Priya'@'%' ;
-
--- Now create user with prop privileges
-CREATE USER 'Priya'@'%' IDENTIFIED BY 'cogo';
-
-GRANT ALL PRIVILEGES ON * . * TO 'Priya'@'%';
